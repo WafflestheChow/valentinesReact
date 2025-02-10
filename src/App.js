453362bea1f671efd365f2d3ea635e66dca8f765
Nick <nick.tsang5@gmail.com>
2025-02-10 12:29:33 -0500

    Refactored 'No' button styling and added Tenor embed container

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import "./App.css";
 
@@ -8,11 +8,10 @@ const messages = [
     "Think again Chunks!",
     "Don't break my heart 😢",
     "You're making a mistake!",
-    "Last chance!!!", // Second to last message (index -2)
+    "Last chance!!!", // Second to last message
 ];
 
 const finalMessage = "Fine... I didn't want to be your Valentine anyways! 😭😭😭";
-const finalGifUrl = "https://media.tenor.com/oqgO-xIbYXIAAAAM/sad-crying.gif"; // Tenor GIF URL
 
 // Predefined safe positions
 const positions = [
@@ -31,22 +30,43 @@ function App() {
     const [hasMoved, setHasMoved] = useState(false);
 
     const handleNoClick = () => {
-        if (messageIndex === messages.length - 1) {
-            // If "Last chance!!!" is reached, stop moving and show final message
-            setShowFinalMessage(true);
-        } else {
+        if (messageIndex < messages.length - 1) {
             setHasMoved(true);
             setPositionIndex((prev) => (prev + 1) % positions.length);
             setMessageIndex((prev) => prev + 1);
+        } else {
+            // Once "Last chance!!!" is clicked, show the final message
+            setShowFinalMessage(true);
         }
     };
+
+    // Load Tenor Embed Script when the final message is displayed
+    useEffect(() => {
+        if (showFinalMessage) {
+            const script = document.createElement("script");
+            script.src = "https://tenor.com/embed.js";
+            script.async = true;
+            document.body.appendChild(script);
+        }
+    }, [showFinalMessage]);
 
     return (
         <div className="container">
             {showFinalMessage ? (
                 <div className="final-message">
                     <h1>{finalMessage}</h1>
-                    <img src={finalGifUrl} alt="Crying Penguin" className="final-gif" />
+                    <div
+                        className="tenor-gif-embed"
+                        data-postid="8932804926861292727"
+                        data-share-method="host"
+                        data-aspect-ratio="1"
+                        data-width="250px"
+                    >
+                        <a href="https://tenor.com/view/sad-crying-cry-penguin-tears-gif-8932804926861292727">
+                            Sad Crying GIF
+                        </a>
+                        from <a href="https://tenor.com/search/sad-gifs">Sad GIFs</a>
+                    </div>
                 </div>
             ) : (
                 <>
@@ -56,10 +76,12 @@ function App() {
                             Yes 💕
                         </button>
                         {messageIndex === messages.length - 1 ? (
+                            // Final "No" button - no movement, just clickable
                             <button className="no-button" onClick={handleNoClick}>
                                 {messages[messageIndex]}
                             </button>
                         ) : (
+                            // Moving "No" button until the last message
                             <motion.button
                                 className="no-button"
                                 onClick={handleNoClick}
